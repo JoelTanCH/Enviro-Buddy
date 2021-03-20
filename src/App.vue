e1b861995ead995f278b17d60c9478d904b99e1f
hawjx <67353438+hawjx@users.noreply.github.com>
2021-03-20 22:39:45 +0800

    add routes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
 <template>
   <div id="app">
     <app-header></app-header>
-    
-    <router-view></router-view>    
-
+    <router-view></router-view>
     <app-footer></app-footer>
-   </div>
+  </div>
 </template>
 
 <script>
-//Register Locally
-
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
- data(){
-    return{
-      title:'Your first Vue Component',
-      
-    }
+  data() {
+    return {};
   },
-   methods:{
-     greeting:function(){
-       return 'Week 6 of BT3103';
-     }
-   },
-   //Register Locally
-  components:{
-    'app-header':Header,
-    'app-footer':Footer
-    
-  }
-
-}
+  methods: {},
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;  
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #e48257;
-  font-size:14px;
+  font-size: 14px;
 }
-
+body {
+  background-color: #f2edd7;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,48 +1,43 @@
 <template>
-   <div>
+  <div>
     <header>
-        <h1>{{ msg }}</h1>
-        <router-link to="/mkt-category" exact>Marketplace </router-link>
-        <!-- <router-link to="/event-category" exact>Events </router-link> -->
-        <!-- <router-link to="/info-category" exact>Information Hub</router-link> -->
+      <h1>Enviro Buddy</h1>
+      <router-link to="/mkt-category" exact>Marketplace </router-link>
+      <router-link to="/event-category" exact>Events </router-link>
+      <router-link to="/info-category" exact>Information Hub</router-link>
     </header>
-    </div>
-  
+  </div>
 </template>
 
 <script>
 export default {
-  
-  data(){
-    return{
-        msg: 'Online Bakery'
-        }
-  }
-}
+  data() {
+    return {};
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-header{
-    background: #3a6351;
-    border-style: solid;
-    border-color: #393232;
-    border-width: 2px;
-    height: 95px;
-    text-align: top;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+header {
+  background: #3a6351;
+  border-style: solid;
+  border-color: #393232;
+  border-width: 2px;
+  height: 95px;
+  text-align: top;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
-h1{
-    color: #f2edd7
+h1 {
+  color: #f2edd7;
 }
-a{
-    color: #f2edd7;
-    text-decoration: none;
-    padding: 6px 8px;
-    border-radius: 10px;
+a {
+  color: #f2edd7;
+  text-decoration: none;
+  padding: 6px 8px;
+  border-radius: 10px;
 }
-.router-link-active{
-    background: #f2edd7;
-    color: #393232;
+.router-link-active {
+  background: #f2edd7;
+  color: #393232;
 }
 </style>
