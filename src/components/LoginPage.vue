54d6687f45bc4e144a49e5008e2618be283fd114
jaredtin98 <80029641+jaredtin98@users.noreply.github.com>
2021-03-23 23:40:24 +0800

    Added front end code for Login & Sign up Page and edit Root Page

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- need background image??? or anything else to add -->
+    <div id = 'container'>
+    </div>
+</template>
+
+<style scoped>
+#container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+</style>
