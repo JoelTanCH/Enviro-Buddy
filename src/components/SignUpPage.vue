d91653f9fcaa85cfd94e31662cb611d2f8ce285a
hawjx <67353438+hawjx@users.noreply.github.com>
2021-04-12 17:59:57 +0800

    standardised css

--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,57 +1,60 @@
 <template>
   <div id="main">
-    <b-container>
-      <b-row>
-        <b-col>
-          <img id="logo" v-bind:src="logoURL" />
-        </b-col>
-        <b-col>
-          <h1>Sign Up</h1> <br>
+    <div id="content">
+      <img id="logo" v-bind:src="logoURL" />
+      <h1>Sign Up</h1>
+      <br />
 
-          <b-form>
-            <b-form-input
-              class="inputField"
-              type="text"
-              placeholder="Username"
-              required
-              v-model='username'
-            >
-            </b-form-input>
-            <b-form-input
-              class="inputField"
-              type="text"
-              placeholder="Email"
-              required
-              v-model='email'
-            >
-            </b-form-input>
-            <b-form-input
-              class="inputField"
-              type="password"
-              placeholder="Password"
-              required
-              v-model='password'
-            >
-            </b-form-input>
-            <!-- include check for password here -->
-            <b-form-input
-              class="inputField"
-              type="password"
-              placeholder="Confirm Password"
-              required
-              v-model='confirmPassword'
-            >
-            </b-form-input>
-          </b-form>
+      <b-form>
+        <b-form-input
+          class="inputField"
+          type="text"
+          placeholder="Username"
+          required
+          v-model="username"
+        >
+        </b-form-input>
+        <b-form-input
+          class="inputField"
+          type="text"
+          placeholder="Email"
+          required
+          v-model="email"
+        >
+        </b-form-input>
+        <b-form-input
+          class="inputField"
+          type="password"
+          placeholder="Password"
+          required
+          v-model="password"
+        >
+        </b-form-input>
+        <!-- include check for password here -->
+        <b-form-input
+          class="inputField"
+          type="password"
+          placeholder="Confirm Password"
+          required
+          v-model="confirmPassword"
+        >
+        </b-form-input>
+      </b-form>
 
-          <b-button class="button" v-on:click="createUser" variant = "outline-success"> Register </b-button><br>
-          <span>Already have an account?</span>
-          <b-button variant="text" class="textButton" v-on:click="routeLogin"
-            >Log In</b-button>
-          
-        </b-col>
-      </b-row>
-    </b-container>
+      <b-button
+        class="button"
+        v-on:click="createUser"
+        variant="outline-success"
+      >
+        Register </b-button
+      ><br />
+
+      <div>
+        <span>Already have an account? </span>
+        <a href="/login">Log In</a>
+      </div>
+    </div>
+    <img id="bg-img" v-bind:src="backgroundURL" />
   </div>
 </template>
 
@@ -62,57 +65,65 @@ import "firebase/auth";
 export default {
   data() {
     return {
-      username:"",
+      username: "",
       email: "",
       password: "",
       confirmPassword: "",
       usernameTaken: false,
       logoURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpYToeTlE4lcmsSPc7e18gnrH9xnf3HGCrGOl9qOP4ez8ziM2-ROBNAc-T6cESI7V_btc&usqp=CAU",
+        "https://peppertreevenue.co.za/wp-content/uploads/2019/04/pepper-tree-favicon-transparent.png",
+      backgroundURL:
+        "https://images.unsplash.com/photo-1536882240095-0379873feb4e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
     };
   },
   methods: {
-    routeLogin: function () {
-      this.$router.push({ path: "/login" });
+    routeRegister: function () {
+      this.$router.push({ path: "/confirm-signup" });
     },
 
-    routeRegister: function() {
-      this.$router.push({path: '/confirm-signup'})
-    },
-
-    createUser: function() {   
+    createUser: function () {
       if (this.password != this.confirmPassword) {
         alert("Passwords do not match");
       } else if (this.username == "") {
         alert("Invalid username");
       } else {
-        database.collection("users").get().then(querySnapshot => {
-          querySnapshot.docs.forEach(doc => {
-          var data = doc.data();
-            if (data.username == this.username) {
-              this.usernameTaken = true;
-            }
+        database
+          .collection("users")
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.docs.forEach((doc) => {
+              var data = doc.data();
+              if (data.username == this.username) {
+                this.usernameTaken = true;
+              }
+            });
           })
-        }).then(() => {
-          if (this.usernameTaken) {
-            alert("This username has been taken");
-            this.usernameTaken = false;
-          } else {
-            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
-            .then(() => firebase.auth().currentUser.sendEmailVerification())
-            .then(() => database.collection("users").doc(this.email).set({
-              username: this.username}))
-            .then(() => {
-              this.username = "";
-              this.email = "";
-              this.password = "";
-              this.confirmPassword = ""; 
+          .then(() => {
+            if (this.usernameTaken) {
+              alert("This username has been taken");
               this.usernameTaken = false;
-              this.routeRegister();})        
-            .catch(err => alert(err.message))
-          }
-        })
-      }  
+            } else {
+              firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(() => firebase.auth().currentUser.sendEmailVerification())
+                .then(() =>
+                  database.collection("users").doc(this.email).set({
+                    username: this.username,
+                  })
+                )
+                .then(() => {
+                  this.username = "";
+                  this.email = "";
+                  this.password = "";
+                  this.confirmPassword = "";
+                  this.usernameTaken = false;
+                  this.routeRegister();
+                })
+                .catch((err) => alert(err.message));
+            }
+          });
+      }
     },
   },
 };
@@ -120,7 +131,7 @@ export default {
 
 <style scoped>
 #logo {
-  width: 70%;
+  width: 25%;
   align-items: center;
 }
 h1 {
@@ -128,18 +139,43 @@ h1 {
 }
 .inputField {
   font-size: 16px;
-  margin: 10px;
+  margin-bottom: 10px;
+  width: 300px;
+  margin-left: auto;
+  margin-right: auto;
 }
 span {
   font-size: 16px;
   color: #393232;
 }
-.textButton {
-  margin-top:-3px;
-  margin-left:-7px;
-  font-size: 16px;
+a {
+  color: #3a6351;
+  font-weight: bolder;
+}
+a:hover {
   color: #e48257;
-  font-weight: bold;
+  text-decoration: none;
+}
+.button {
+  font-size: 16px;
+  margin-bottom: 10px;
+  width: 240px;
+}
+#content {
+  float: left;
+  padding-bottom: 5%;
+}
+#bg-img {
+  opacity: 0.5;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
 
