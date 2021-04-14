d5d6218372f0f55cb242b6862e38d8f76269f07d
JoelTanCH <80249497+JoelTanCH@users.noreply.github.com>
2021-04-14 18:26:58 +0800

    able to see how many people like a post &increment

--- src/components/InfoDetails.vue
+++ src/components/InfoDetails.vue
@@ -7,8 +7,12 @@
         </b-col>
         <b-col>
           <h2>{{ item.name }}</h2>
-          <h5>Posted by: {{item.username}}</h5>
-          <p id = "desc"> {{item.description}} </p>
+          <h5>Posted by: {{ item.username }}</h5>
+          <p id="desc">{{ item.description }}</p>
+          <template v-if="show">
+            <b-button v-on:click="like"> like </b-button>
+          </template>
+          <h5>{{ item.likes }}  person/people liked this!</h5>
         </b-col>
       </b-row>
     </b-container>
@@ -17,22 +21,38 @@
 
 <script>
 import database from "../firebase.js";
+import firebase from "firebase/app";
 
 export default {
   data() {
     return {
+      show: true,
       item: {},
       itemid: "",
       subCollectionName: "",
     };
   },
-  components: {
+  components: {},
+  methods: { // have not restricted to 1 acc 1 like yet 
+    like: function () {
 
-  },
-  methods: {
+      const increment = firebase.firestore.FieldValue.increment(1);
+      this.show = !this.show;
+
+      database
+        .collection("info-categories")
+        .doc(this.subCollectionName)
+        .collection("items")
+        .doc(this.itemid)
+        .update({
+          likes: increment,
+        });
+    },
     fetchItems: function () {
       this.itemid = this.$route.params.itemid;
       this.subCollectionName = this.$route.params.subCollectionName;
+      console.log("itemid: " + this.itemid);
+      console.log("subcollection: " + this.subCollectionName);
 
       database
         .collection("info-categories")
@@ -41,14 +61,12 @@ export default {
         .doc(this.itemid)
         .get()
         .then((snapshot) => {
-          this.item = snapshot.data()
-          console.log(this.item.description)
-          var text = this.item.description
+          this.item = snapshot.data();
+          var text = this.item.description;
           text = text.replaceAll("\\n", "\n");
-          this.item.description=text;
-          console.log(text)
+          this.item.description = text;
         });
-    }
+    },
   },
   created: function () {
     this.fetchItems();
@@ -65,9 +83,9 @@ img {
 .username {
   color: #e48257;
 }
-#desc{
+#desc {
   white-space: pre-line;
-  text-align:left;
+  text-align: left;
 }
 p {
   text-align: center;
@@ -75,7 +93,7 @@ p {
   border: 1px solid #e48257;
   margin: 1%;
   width: 85%;
-  height:85%;
-  background-color:  #e48257; 
+  height: 85%;
+  background-color: #e48257;
 }
 </style>
