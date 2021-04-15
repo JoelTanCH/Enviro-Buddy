95f896d8c414169ba9d8422f1e44d56b452e450a
hawjx <67353438+hawjx@users.noreply.github.com>
2021-04-15 14:26:45 +0800

    Update Profile.vue
    edited css for profile info

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,70 +1,70 @@
 <template>
   <div>
-    <section class="section">
-      <div class="container-fliud">
+    <section id="user-info">
+      <div id="profile-pic-container">
         <img
-          class="profpic"
-          contain
-          height="300px"
-          width="300px"
+          id="profile-pic"
           src="https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
         />
-        <h1>{{ this.user.username }}</h1>
-        <h5>{{ this.email }}</h5>
-
-        <b-tabs content-class="mt-3">
-          <b-tab title="My Marketplace Listings" active>
-            <ul>
-              <li v-for="item in mymktlist" v-bind:key="item.name">
-                <div>
-                  <h2>{{ item.name }}</h2>
-                  <div>$ {{ item.price }} / item</div>
-                  <div>Quantity Sold: {{ item.quantitySold }}</div>
-                  <img v-bind:src="item.img" />
-                </div>
-              </li>
-            </ul>
-          </b-tab>
-
-          <b-tab title="My Events" active>
-            <ul>
-              <li v-for="event in eventlist" v-bind:key="event.name">
-                <div>
-                  <h2>{{ event.name }}</h2>
-                  <div>{{ event.date.toDate() }}</div>
-                  <div>Location: {{ event.location }}</div>
-                  <img v-bind:src="event.img" />
-                </div>
-              </li>
-            </ul>
-          </b-tab>
-
-          <b-tab title="My Purchase History" active>
-            <ul>
-              <li v-for="item in purchasedlist" v-bind:key="item.name">
-                <div>
-                  <h2>{{ item.name }}</h2>
-                  <div>$ {{ item.price }} / item</div>
-                  <div>Quantity: {{ item.quantity }}</div>
-                  <img v-bind:src="item.img" />
-                </div>
-              </li>
-            </ul>
-          </b-tab>
-
-          <b-tab title="My Infohub Posts" active>
-            <ul>
-              <li v-for="item in infolist" v-bind:key="item.name">
-                <div>
-                  <h2>{{ item.name }}</h2>
-                  <div class="description">{{ item.description }}</div>
-                </div>
-                <img v-bind:src="item.img" />
-              </li>
-            </ul>
-          </b-tab>
-        </b-tabs>
       </div>
+
+      <h1>{{ this.user.username }}</h1>
+      <h5>{{ this.email }}</h5>
+    </section>
+
+    <section id="user-acivities">
+      <b-tabs id="tabs">
+        <b-tab title="My Marketplace Listings" active>
+          <ul>
+            <li v-for="item in mymktlist" v-bind:key="item.name">
+              <div>
+                <h2>{{ item.name }}</h2>
+                <div>$ {{ item.price }} / item</div>
+                <div>Quantity Sold: {{ item.quantitySold }}</div>
+                <img v-bind:src="item.img" />
+              </div>
+            </li>
+          </ul>
+        </b-tab>
+
+        <b-tab title="My Events" active>
+          <ul>
+            <li v-for="event in eventlist" v-bind:key="event.name">
+              <div>
+                <h2>{{ event.name }}</h2>
+                <div>{{ event.date.toDate() }}</div>
+                <div>Location: {{ event.location }}</div>
+                <img v-bind:src="event.img" />
+              </div>
+            </li>
+          </ul>
+        </b-tab>
+
+        <b-tab title="My Purchase History" active>
+          <ul>
+            <li v-for="item in purchasedlist" v-bind:key="item.name">
+              <div>
+                <h2>{{ item.name }}</h2>
+                <div>$ {{ item.price }} / item</div>
+                <div>Quantity: {{ item.quantity }}</div>
+                <img v-bind:src="item.img" />
+              </div>
+            </li>
+          </ul>
+        </b-tab>
+
+        <b-tab title="My Infohub Posts" active>
+          <ul>
+            <li v-for="item in infolist" v-bind:key="item.name">
+              <div>
+                <h2>{{ item.name }}</h2>
+                <div class="description">{{ item.description }}</div>
+              </div>
+              <img v-bind:src="item.img" />
+            </li>
+          </ul>
+        </b-tab>
+      </b-tabs>
     </section>
   </div>
 </template>
@@ -170,13 +170,18 @@ export default {
 
 
 
-<style lang="css" scoped>
-.profpic {
+<style scoped>
+#profile-pic-container {
+  width: 100px;
+  height: 100px;
+  overflow: hidden;
   border-radius: 50%;
-  width: 26%;
 }
-body {
-  background-color: #f2edd7;
+#profile-pic {
+  display: inline;
+  margin: 0 auto;
+  height: 100%;
+  width: auto;
 }
 ul {
   display: flex;
@@ -195,11 +200,6 @@ img {
   height: 300px;
   width: 90%;
   object-fit: cover;
-}
-#searchbar-container {
-  width: 31.3%;
-  margin-right: 1%;
-  margin-left: auto;
 }
 .price {
   color: #3a6351;
@@ -222,5 +222,9 @@ img {
   line-height: 1.5em;
   height: 4.5em;
   width: 90%;
+}
+#user-info * {
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
