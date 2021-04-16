8f8b224cc93c3197c8d3d9cdacbc7b9f35397de2
JoelTanCH <80249497+JoelTanCH@users.noreply.github.com>
2021-04-16 16:36:00 +0800

    edited code to ensure permanent heart type

--- src/components/InfoDetails.vue
+++ src/components/InfoDetails.vue
@@ -9,7 +9,7 @@
           <h2>{{ item.name }}</h2>
           <h5>Posted by: {{ item.username }}</h5>
           <p id="desc">{{ item.description }}</p>
-          <template v-if="show">
+          <div v-if="show">
             <b-button v-on:click="like">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -24,9 +24,9 @@
                 />
               </svg>
             </b-button>
-          </template>
-          <template v-else>
-            <b-button>
+          </div>
+          <div v-else>
+            <b-button v-on:click='like'>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -41,7 +41,7 @@
                 />
               </svg>
             </b-button>
-          </template>
+          </div>
 
           <h5>{{ item.likes }} person/people liked this!</h5>
         </b-col>
@@ -57,16 +57,7 @@ import firebase from "firebase/app";
 export default {
   data() {
     return {
-      emptyHeart: {
-        d:
-          "M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z",
-      },
-      filledHeart: {
-        fillRule: "evenodd",
-        d:
-          "m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z",
-      },
-      show: true,
+      show:null,
       likedBefore: [],
       item: {},
       itemid: "",
@@ -140,7 +131,18 @@ export default {
           this.likedBefore.push(doc.data().email);
           console.log("likedBefore : " + this.likedBefore);
         });
-      });
+      })
+      .then(()=> {
+        var currentUser = firebase.auth().currentUser.email
+        console.log("liked Before next: "+ this.likedBefore)
+        console.log(currentUser)
+        if(this.likedBefore.includes(currentUser)){
+          this.show = false //alr liked before
+        }else{
+          this.show = true //have not liked before
+        }
+      })
+      ;
   },
 };
 </script>
