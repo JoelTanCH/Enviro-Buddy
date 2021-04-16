78f4dee4021361d662aa192ac105d81caec620b4
JoelTanCH <80249497+JoelTanCH@users.noreply.github.com>
2021-04-16 16:14:34 +0800

    info hub like botton is now a heart icon

--- src/components/InfoDetails.vue
+++ src/components/InfoDetails.vue
@@ -10,8 +10,39 @@
           <h5>Posted by: {{ item.username }}</h5>
           <p id="desc">{{ item.description }}</p>
           <template v-if="show">
-            <b-button v-on:click="like"> like </b-button>
+            <b-button v-on:click="like">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-heart"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
+                />
+              </svg>
+            </b-button>
           </template>
+          <template v-else>
+            <b-button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-heart-fill"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                />
+              </svg>
+            </b-button>
+          </template>
+
           <h5>{{ item.likes }} person/people liked this!</h5>
         </b-col>
       </b-row>
@@ -26,8 +57,17 @@ import firebase from "firebase/app";
 export default {
   data() {
     return {
+      emptyHeart: {
+        d:
+          "M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z",
+      },
+      filledHeart: {
+        fillRule: "evenodd",
+        d:
+          "m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z",
+      },
       show: true,
-      likedBefore:[],
+      likedBefore: [],
       item: {},
       itemid: "",
       subCollectionName: "",
@@ -37,11 +77,10 @@ export default {
   methods: {
     like: function () {
       var currentUser = firebase.auth().currentUser.email; //current user email
-      console.log(this.likedBefore)
-      
+      console.log(this.likedBefore);
+
       if (this.likedBefore.includes(currentUser)) {
         alert("You have liked this already");
-
       } else {
         const increment = firebase.firestore.FieldValue.increment(1);
         this.show = !this.show;
@@ -53,8 +92,8 @@ export default {
           .doc(this.itemid)
           .collection("likedBefore")
           .add({
-            email: currentUser
-            })
+            email: currentUser,
+          });
 
         database
           .collection("info-categories")
@@ -89,19 +128,19 @@ export default {
   created: function () {
     this.fetchItems();
     database
-        .collection("info-categories")
-        .doc(this.subCollectionName)
-        .collection("items")
-        .doc(this.itemid)
-        .collection("likedBefore")
-        .get()
-        .then((querySnapShot) => {
-          querySnapShot.forEach((doc) => {
-            console.log("doc email data: " + doc.data().email);
-            this.likedBefore.push(doc.data().email);
-            console.log("likedBefore : " + this.likedBefore);
-          });
-        });    
+      .collection("info-categories")
+      .doc(this.subCollectionName)
+      .collection("items")
+      .doc(this.itemid)
+      .collection("likedBefore")
+      .get()
+      .then((querySnapShot) => {
+        querySnapShot.forEach((doc) => {
+          console.log("doc email data: " + doc.data().email);
+          this.likedBefore.push(doc.data().email);
+          console.log("likedBefore : " + this.likedBefore);
+        });
+      });
   },
 };
 </script>
